10048c8cfa200d7e2b519a9fdad9c40ae43babcb
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-02 04:55:57 +0000

    add file open cat file function

--- dcat.c
+++ dcat.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <error.h>
+#include <errno.h>
+
+void cat_file(const char *file_path)
+{
+    FILE *f = fopen(file_path, "r");
+    if (f == NULL) {
+        fprintf(stderr, "ERROR: could not open file %s: %s\n", file_path, strerror(errno));
+        exit(1);
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -7,6 +18,8 @@ int main(int argc, char **argv)
         fprintf(stderr, "Usage: dcat <files...>");
         exit(1);
     }
+
+
 
     return 0;
 }
